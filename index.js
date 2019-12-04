7f92a5b37325c06f5f7ea669cb904abbfeb2eeac
Graham Robertson <root@grahamrobertson.forsale>
2019-12-04 12:59:44 +0000

    Bot server with helloworld component. Integration with CoCo.

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+// @ts-nocheck
 const express = require('express'),
 		app = express(),
  		port = process.env.PORT || 3000;
 const request = require("request");
 const { execFile } = require("child_process");
+const fs = require("fs");
 
-const components = require("./components.json");
-const session_id = require("uuid/v1")();
+const BOT_DIR = process.env.COMPONENTS || "./components";
+const REMOTE_BOTS = require("./components.json");
+const SESSION_ID = require("uuid/v1")();
 
 app.listen(port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-console.log(`Server running on port ${port} with session "${session_id}"`);
+console.log(`serving on port ${port}`);
 
 const remote_component = description => (req, res) => {
+	console.log(`User input: "${req.body.user_input}"`);
 	request({
-		url: description.url + session_id,
+		url: description.url + SESSION_ID,
 		method: 'POST',
 		headers: description.headers || {},
 		body: JSON.stringify(req.body)
@@ -23,29 +27,37 @@ const remote_component = description => (req, res) => {
 		if (e)
 			res.send(e);
 		else
-			res.json(JSON.parse(b))
+			console.log('Bot response:', JSON.parse(b)) || res.json(JSON.parse(b));
 	})
 };
 
 const local_component = description => (req, res) => {
-	console.log(req.body);
-	child = execFile(
+	console.log('Executing component:', description.path);
+	console.log(`User input: "${req.body.user_input}"`);
+	execFile(
 		description.path,
-		[JSON.stringify(req.body)],
+		[SESSION_ID, JSON.stringify(req.body)],
 		(error, stdout, stderr) => {
 			if (error)
-				console.error('Error executing', description.path, error);
+				res.send(stderr);
 			else
+				console.log('Bot response:',
+					JSON.parse(stdout)) ||
 				res.json(JSON.parse(stdout));
 		}
 	);
 };
 
-// go through the components and make a route for each one!
-Object.entries(components).forEach(([name, description]) => {
-	console.log(`ADDING COMPONENT ${name}`)
-	app.route(`/${name}`).post(
-		description.url ?
-			remote_component(description) : local_component(description)
-	);
+
+
+Object.entries(REMOTE_BOTS).forEach(([name, description]) => {
+	console.log(`Routing "localhost:${port}/${name}" to ${description.url}`);
+	app.route(`/${name}`).post(remote_component(description));
+});
+
+fs.readdirSync(BOT_DIR).forEach(name => {
+	console.log(`Routing "localhost:${port}/${name}" to ${BOT_DIR}/${name}/`);
+	app.route(`/${name}`).post(local_component({
+		path: `${BOT_DIR}/${name}/index`
+	}));
 });
