56d0bd3b19f9d9da7dac4f967a6bc18a018037a0
Graham Robertson <root@grahamrobertson.forsale>
2019-12-16 14:02:05 +0000

    changed ip to work on hosting

--- clients/bot.js
+++ clients/bot.js
@@ -1,4 +1,4 @@
 // @ts-nocheck
 const client = require("./client");
 module.exports = (bot_name, session_id) =>
-	client(`${process.env.BOT || "http://localhost:3000"}/${bot_name}/${session_id}`);
+	client(`${process.env.BOT || "http://67.207.69.19:3000"}/${bot_name}/${session_id}`);
